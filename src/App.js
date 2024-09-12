a52a13c0b978bb7ff25e07ae8a95164c373eef2e
olae12 <egommise@gmail.com>
2024-09-12 05:29:05 -0700

    about page updatetd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Components/pages/Home";
+import About from "./Components/pages/About"
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
 
           <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+
 
       
           </Routes>
